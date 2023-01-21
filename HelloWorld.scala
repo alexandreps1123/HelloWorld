9f1fdb3f61148352ef805860b3acafc4deee8fc4
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-20 21:18:40 -0300

    function

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -7,5 +7,17 @@ object HelloWorld {
 
     // ternary operator like
     if (a > b) println(a) else println(b)
+
+    def maxSquaredDoubled(a: Int, b: Int): Int = 
+      if (a > b) {
+        val squared = a * a
+        squared * 2
+      } else {
+        val squared = b * b
+        squared * 2
+      }
+
+    println(maxSquaredDoubled(a, b))
+
   }
 }
