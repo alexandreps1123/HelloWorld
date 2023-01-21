8b0085d8c55f60b6c4a22a9701f1a4f113c0a873
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-20 21:17:32 -0300

    ternary operator

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,5 +1,11 @@
 object HelloWorld {
   def main(args: Array[String]) = {
     println("Hello World!")
+
+    val a = 10
+    val b = 20
+
+    // ternary operator like
+    if (a > b) println(a) else println(b)
   }
 }
